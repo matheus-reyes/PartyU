7f60b32b3757eafe2b90d2788e1039bc5cd04011
gaeranmari <mikaelshin98@gmail.com>
2021-05-26 21:53:43 -0300

    adição de uma query + álgebra relacional

--- bd/SQLs/consultas.sql
+++ bd/SQLs/consultas.sql
@@ -1,18 +1,21 @@
 /*
 Consulta 1
-Uma das empresas organizadoras está passando por uma grave crise financeira e não conseguiu patrocínio para a maioria dos eventos realizados. Por 
-isso, decidiu reduzir os custos com a folha de pagamento, definindo os seguintes critérios de seleção para as primeiras demissões: organizadores 
-que não coordenam nenhum mobilizador de caravana, possuem tempo de contrato menor que 6 meses e possuem remuneração a partir de R$720,00.
+Deseja-se saber qual rede social é mais apropriada para a divulgação de um evento razoavelmente custoso (mais de R$5.000) e ocorrido em abril.
 
-Qual o cpf dos Organizadores que não coordenam nenhum mobilizador de caravana, possuem 
-tempo de contrato menor do que 6 meses e possuem remuneração maior do que 720,00.
+Nomes das redes sociais que promoveram eventos com custo superior a 5.000 reais e que tiveram notas emitidas no mês de abril.
+
+Algebra Relacional:
+Joins <- Rede_Social ⋈(Rede_Social.id_evento = Evento.id_evento) 
+Evento ⋈(Evento.id_evento = Movimentacao_Financeira.id_evento) 
+Movimentacao_Financeira ⋈(Movimentacao_Financeira.codigo_movimentacao = Nota_Fiscal.codigo_movimentacao) Nota_Fiscal 
+Resposta <- π(nome σ(valor > 5000 AND data_emissao like '%-04-%'))(Joins)
 */
-SELECT DISTINCT organizador.cpf FROM organizador
-LEFT JOIN mobilizador_caravana
-on organizador.cpf = mobilizador_caravana.COO_CPF
-where mobilizador_caravana.COO_CPF is null and organizador.cpf in
-	(SELECT distinct organizador.cpf from contrata, organizador
-	where datediff(data_fim, data_inicio) < 6*30 AND remuneracao >= 720.00);
+SELECT Rede_Social.nome FROM Rede_Social
+INNER JOIN Evento ON Rede_Social.id_evento = Evento.id_evento 
+INNER JOIN Movimentacao_Financeira ON Evento.id_evento = Movimentacao_Financeira.id_evento 
+INNER JOIN Nota_Fiscal ON Movimentacao_Financeira.codigo_movimentacao = Nota_Fiscal.codigo_movimentacao
+WHERE Nota_Fiscal.valor > 5.000 AND Nota_Fiscal.data_emissao like '%-04-%'
+
 
 /*
 Consulta 2
@@ -21,10 +24,16 @@ de 10 participantes ouvintes. Isso se deve ao fato de que, a partir de 11 partic
 para que nenhum ouvinte se sinta prejudicado e para que o indivíduo que esteja apresentando também não fique prejudicado por falar muito alto.
 
 Liste as atividades que possuam  mais de 10 ouvintes inscritos. 
+
+Algebra Relacional:
+Joins <- atividade ⋈(atividade.codigo_atividade = INSCREVE.codigo_atividade) INSCREVE
+Count <- codigo_atividade ℱ(CONTA cpf)(Joins)
+Resposta <- π(nome, Count σ(Count > 10))(Joins)
 */
 SELECT atividade.nome, COUNT(*) as participantes FROM atividade 
 INNER JOIN INSCREVE ON atividade.codigo_atividade = INSCREVE.codigo_atividade
 GROUP BY INSCREVE.codigo_atividade HAVING COUNT(*) > 10;
+
 
 /*
 Consulta 3
@@ -34,6 +43,12 @@ técnicos-científicos sediados no Nordeste para que possam atuar como patrocina
 gostaria da informação sobre quais desses eventos possuem menos de 3 patrocinadores. 
 
 Quais os nomes dos eventos que ocorrem presencialmente no Ceará e que possuem menos de 3 patrocinadores?
+
+Algebra Relacional:
+J <- evento ⋈(evento.id_evento = PATROCINA.id_evento) PATROCINA
+Joins <- evento ⋈(evento.id_evento = FAZ_LOCACAO.id_evento) FAZ_LOCACAO ⋈(FAZ_LOCACAO.id_local = Local_presencial.id_local) Local_presencial 
+Count <- id_evento ℱ(CONTA cnpj)(J)
+Resposta <- π(id_evento, nome σ(Count < 3 AND end_estado like '%Ceará%'))(Joins)
 */
 SELECT DISTINCT Evento.id_evento, Evento.nome from evento 
 INNER JOIN FAZ_LOCACAO ON evento.id_evento = FAZ_LOCACAO.id_evento
@@ -41,6 +56,7 @@ INNER JOIN Local_presencial on FAZ_LOCACAO.id_local = Local_presencial.id_local
 INNER JOIN PATROCINA on PATROCINA.id_evento = evento.id_evento
 where Local_presencial.end_estado like '%Ceará%'
 GROUP BY PATROCINA.id_evento HAVING COUNT(*)<3;
+
 
 /*
 Consulta 4
@@ -50,8 +66,15 @@ Então é necessário saber qual participante é um ministrante_tutoria, tem sob
 incoerência no banco de dados.
 
 Qual o nome completo e o cpf do participante que é um ministrante_tutoria, possui “Pereira” no seu nome e é também um palestrante?
+
+Algebra Relacional:
+J <- participante ⋈(participante.cpf = ministrante_tutoria.cpf) ministrante_tutoria 
+Joins <- J ⋈(J.cpf = palestrante.cpf) palestrante
+Resposta <- π(nome σ(nome like '%Pereira%'))(Joins)
 */
-select ministrante.nome, ministrante.cpf from participante as ministrante
+SELECT ministrante.nome, ministrante.cpf from participante as ministrante
 INNER JOIN ministrante_tutoria on ministrante.cpf = ministrante_tutoria.cpf
 INNER JOIN palestrante on palestrante.cpf = ministrante.cpf
-where nome like '%Pereira%';
+WHERE nome like '%Pereira%';
+
+
